dcfb15f47a0dd01fee99a79e8592659df52327fe
csorgopeter <peter199990511@gmail.com>
2022-05-18 23:17:25 +0200

    query lenght test #16

--- features/f1_steps.js
+++ features/f1_steps.js
@@ -2,6 +2,7 @@ const { Before, Given, When, Then } = require('@cucumber/cucumber')
 const Url = require('../url.js');
 const assert = require('assert');
 const { Stream } = require('stream');
+const { count } = require('console');
 
 var url = new Url();
 var url_check = new Url();
@@ -17,3 +18,16 @@ Then('the URL will be {string}', function (string)
     url_check = new Url(string)
     assert.equal(url_check.toString(), url.toString());
 });
+
+
+Given('The URL is {string}', function (string){
+    url = new Url(string)
+});
+Given('I count the string params', function() {
+     var count = url.queryLength()
+});
+Then('It should be {int}', function (int)
+{
+    count === int
+});
+
--- features/t1.feature
+++ features/t1.feature
@@ -1,6 +1,19 @@
-Feature: clear query
 
-Scenario: No query params left
+Feature: Manipulating URLs
+
+Scenario: Removes all query string parameters from the URL
 Given An URL is "http://example.com/?a&a=&b=&c=&d=&e=&f=&g=&h#foo"
 And I delete the query parameters
 Then the URL will be "http://example.com/#foo"
+
+
+Scenario: Returns total count of the query string parameters
+Given The URL is "http://example.com/?a"
+And I count the string params
+Then It should be 1
+Given The URL is "http://example.com/?*342332"
+And I count the string params
+Then It should be 0
+Given The URL is "http://example.com/?a232323Ab"
+And I count the string params
+Then It should be 3
